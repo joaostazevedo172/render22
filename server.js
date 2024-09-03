e11745a4350179ed1e7cdec2c3694cd9b6fa34d7
João Pedro Azevedp <113111839+joaostazevedo172@users.noreply.github.com>
2024-09-03 20:29:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json()); // Adicionei para suportar JSON no corpo da requisi�
 
 // Configuração da conexão com o banco de dados
 const con = mysql.createConnection({
-    host: process.env.DB_HOST || " sql110.byetcluster.com ",
+    host: process.env.DB_HOST || "b33_37133203_render",
     user: process.env.DB_USER || "b33_37133203",
     password: process.env.DB_PASSWORD || "Epa2548J@",
     database: process.env.DB_NAME || "b33_37133203_render",
